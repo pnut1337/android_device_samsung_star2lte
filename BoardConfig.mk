71aa856997e1d3b5f70a04fdcd67bd7c61a6b1a0
pnut1337 <38246958+pnut1337@users.noreply.github.com>
2026-03-22 20:38:10 +0700

    Add TARGET_SUPPORTS_64_BIT_APPS for OrangeFox 12.1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,6 +10,7 @@ TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
+TARGET_SUPPORTS_64_BIT_APPS := true
 
 # Board
 BOARD_VENDOR := samsung
